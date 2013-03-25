e92f5ca750764458eea26bc8ae97b7798ca875fc
Faraz Fallahi <fffaraz@gmail.com>
2013-03-25 18:29:24 +0430

    saman ferestande

--- include/request_ferestande.php
+++ include/request_ferestande.php
@@ -1057,7 +1057,9 @@ class PEAR_Error
  * c-basic-offset: 4
  * End:
  */
+ ?>
  
+ <?php
 /**
  * This is only meant for PHP 5 to get rid of certain strict warning
  * that doesn't get hidden since it's in the shutdown function
@@ -1090,6 +1092,9 @@ class PEAR5
         return $properties[$class][$var];
     }
 }
+?>
+
+<?php
 //
 // +----------------------------------------------------------------------+
 // | PHP Version 4                                                        |
@@ -1681,6 +1686,9 @@ class Net_Socket extends PEAR {
     }
 
 }
+?>
+
+<?php
 // +-----------------------------------------------------------------------+
 // | Copyright (c) 2002-2004, Richard Heyes                                |
 // | All rights reserved.                                                  |
@@ -2164,6 +2172,14 @@ class Net_URL
     }
 
 }
+?>
+
+
+
+
+
+
+<?php
 /**
  * Class for performing HTTP requests
  *
@@ -3284,94 +3300,29 @@ class HTTP_Request
  */
 class HTTP_Response
 {
-    /**
-    * Socket object
-    * @var Net_Socket
-    */
     var $_sock;
-
-    /**
-    * Protocol
-    * @var string
-    */
     var $_protocol;
-
-    /**
-    * Return code
-    * @var string
-    */
-    var $_code;
-
-    /**
-    * Response reason phrase
-    * @var string
-    */
-    var $_reason;
-
-    /**
-    * Response headers
-    * @var array
-    */
+	var $_code;
+	var $_reason;
+	
     var $_headers;
-
-    /**
-    * Cookies set in response
-    * @var array
-    */
+	
     var $_cookies;
-
-    /**
-    * Response body
-    * @var string
-    */
+	
     var $_body = '';
-
-   /**
-    * Used by _readChunked(): remaining length of the current chunk
-    * @var string
-    */
+	
     var $_chunkLength = 0;
-
-   /**
-    * Attached listeners
-    * @var array
-    */
+	
     var $_listeners = array();
-
-   /**
-    * Bytes left to read from message-body
-    * @var null|int
-    */
+	
     var $_toRead;
-
-    /**
-    * Constructor
-    *
-    * @param  Net_Socket    socket to read the response from
-    * @param  array         listeners attached to request
-    */
+	
     function HTTP_Response(&$sock, &$listeners)
     {
         $this->_sock      =& $sock;
         $this->_listeners =& $listeners;
     }
-
-
-   /**
-    * Processes a HTTP response
-    *
-    * This extracts response code, headers, cookies and decodes body if it
-    * was encoded in some way
-    *
-    * @access public
-    * @param  bool      Whether to store response body in object property, set
-    *                   this to false if downloading a LARGE file and using a Listener.
-    *                   This is assumed to be true if body is gzip-encoded.
-    * @param  bool      Whether the response can actually have a message-body.
-    *                   Will be set to false for HEAD requests.
-    * @throws PEAR_Error
-    * @return mixed     true on success, PEAR_Error in case of malformed response
-    */
+	
     function process($saveBody = true, $canHaveBody = true)
     {
         do {
@@ -3389,17 +3340,10 @@ class HTTP_Response
         } while (100 == $this->_code);
 
         $this->_notify('gotHeaders', $this->_headers);
-
-        // RFC 2616, section 4.4:
-        // 1. Any response message which "MUST NOT" include a message-body ...
-        // is always terminated by the first empty line after the header fields
-        // 3. ... If a message is received with both a
-        // Transfer-Encoding header field and a Content-Length header field,
-        // the latter MUST be ignored.
+		
         $canHaveBody = $canHaveBody && $this->_code >= 200 &&
                        $this->_code != 204 && $this->_code != 304;
-
-        // If response body is present, read it and decode
+					   
         $chunked = isset($this->_headers['transfer-encoding']) && ('chunked' == $this->_headers['transfer-encoding']);
         $gzipped = isset($this->_headers['content-encoding']) && ('gzip' == $this->_headers['content-encoding']);
         $hasBody = false;
@@ -3433,7 +3377,6 @@ class HTTP_Response
         }
 
         if ($hasBody) {
-            // Uncompress the body if needed
             if ($gzipped) {
                 $body = $this->_decodeGzip($this->_body);
                 if (PEAR::isError($body)) {
@@ -3447,14 +3390,7 @@ class HTTP_Response
         }
         return true;
     }
-
-
-   /**
-    * Processes the response header
-    *
-    * @access private
-    * @param  string    HTTP header
-    */
+	
     function _processHeader($header)
     {
         if (false === strpos($header, ':')) {
@@ -3474,14 +3410,7 @@ class HTTP_Response
             $this->_parseCookie($headervalue);
         }
     }
-
-
-   /**
-    * Parse a Set-Cookie header to fill $_cookies array
-    *
-    * @access private
-    * @param  string    value of Set-Cookie header
-    */
+	
     function _parseCookie($headervalue)
     {
         $cookie = array(
@@ -3490,14 +3419,11 @@ class HTTP_Response
             'path'    => null,
             'secure'  => false
         );
-
-        // Only a name=value pair
+		
         if (!strpos($headervalue, ';')) {
             $pos = strpos($headervalue, '=');
             $cookie['name']  = trim(substr($headervalue, 0, $pos));
             $cookie['value'] = trim(substr($headervalue, $pos + 1));
-
-        // Some optional parameters are supplied
         } else {
             $elements = explode(';', $headervalue);
             $pos = strpos($elements[0], '=');
@@ -3525,24 +3451,17 @@ class HTTP_Response
         }
         $this->_cookies[] = $cookie;
     }
-
-
-   /**
-    * Read a part of response body encoded with chunked Transfer-Encoding
-    *
-    * @access private
-    * @return string
-    */
+	
     function _readChunked()
     {
-        // at start of the next chunk?
+	
         if (0 == $this->_chunkLength) {
             $line = $this->_sock->readLine();
             if (preg_match('/^([0-9a-f]+)/i', $line, $matches)) {
                 $this->_chunkLength = hexdec($matches[1]);
-                // Chunk with zero length indicates the end
+				
                 if (0 == $this->_chunkLength) {
-                    $this->_sock->readLine(); // make this an eof()
+                    $this->_sock->readLine();
                     return '';
                 }
             } else {
@@ -3552,39 +3471,18 @@ class HTTP_Response
         $data = $this->_sock->read($this->_chunkLength);
         $this->_chunkLength -= HTTP_REQUEST_MBSTRING? mb_strlen($data, 'iso-8859-1'): strlen($data);
         if (0 == $this->_chunkLength) {
-            $this->_sock->readLine(); // Trailing CRLF
+            $this->_sock->readLine();
         }
         return $data;
     }
-
-
-   /**
-    * Notifies all registered listeners of an event.
-    *
-    * @param    string  Event name
-    * @param    mixed   Additional data
-    * @access   private
-    * @see HTTP_Request::_notify()
-    */
+	
     function _notify($event, $data = null)
     {
         foreach (array_keys($this->_listeners) as $id) {
             $this->_listeners[$id]->update($this, $event, $data);
         }
     }
-
-
-   /**
-    * Decodes the message-body encoded by gzip
-    *
-    * The real decoding work is done by gzinflate() built-in function, this
-    * method only parses the header and checks data for compliance with
-    * RFC 1952
-    *
-    * @access   private
-    * @param    string  gzip-encoded data
-    * @return   string  decoded data
-    */
+	
     function _decodeGzip($data)
     {
         if (HTTP_REQUEST_MBSTRING) {
@@ -3592,7 +3490,7 @@ class HTTP_Response
             mb_internal_encoding('iso-8859-1');
         }
         $length = strlen($data);
-        // If it doesn't look like gzip-encoded data, don't bother
+		
         if (18 > $length || strcmp(substr($data, 0, 2), "\x1f\x8b")) {
             return $data;
         }
@@ -3604,10 +3502,9 @@ class HTTP_Response
         if ($flags & 224) {
             return PEAR::raiseError('_decodeGzip(): reserved bits are set', HTTP_REQUEST_ERROR_GZIP_DATA);
         }
-
-        // header is 10 bytes minimum. may be longer, though.
+		
         $headerLength = 10;
-        // extra fields, need to skip 'em
+		
         if ($flags & 4) {
             if ($length - $headerLength - 2 < 8) {
                 return PEAR::raiseError('_decodeGzip(): data too short', HTTP_REQUEST_ERROR_GZIP_DATA);
@@ -3618,7 +3515,7 @@ class HTTP_Response
             }
             $headerLength += $extraLength[1] + 2;
         }
-        // file name, need to skip that
+		
         if ($flags & 8) {
             if ($length - $headerLength - 1 < 8) {
                 return PEAR::raiseError('_decodeGzip(): data too short', HTTP_REQUEST_ERROR_GZIP_DATA);
@@ -3629,7 +3526,7 @@ class HTTP_Response
             }
             $headerLength += $filenameLength + 1;
         }
-        // comment, need to skip that also
+		
         if ($flags & 16) {
             if ($length - $headerLength - 1 < 8) {
                 return PEAR::raiseError('_decodeGzip(): data too short', HTTP_REQUEST_ERROR_GZIP_DATA);
@@ -3640,7 +3537,7 @@ class HTTP_Response
             }
             $headerLength += $commentLength + 1;
         }
-        // have a CRC for header. let's check
+		
         if ($flags & 1) {
             if ($length - $headerLength - 2 < 8) {
                 return PEAR::raiseError('_decodeGzip(): data too short', HTTP_REQUEST_ERROR_GZIP_DATA);
@@ -3652,13 +3549,11 @@ class HTTP_Response
             }
             $headerLength += 2;
         }
-        // unpacked data CRC and size at the end of encoded data
+		
         $tmp = unpack('V2', substr($data, -8));
         $dataCrc  = $tmp[1];
         $dataSize = $tmp[2];
 
-        // finally, call the gzinflate() function
-        // don't pass $dataSize to gzinflate, see bugs #13135, #14370
         $unpacked = gzinflate(substr($data, $headerLength, -8));
         if (false === $unpacked) {
             return PEAR::raiseError('_decodeGzip(): gzinflate() call failed', HTTP_REQUEST_ERROR_GZIP_READ);
@@ -3672,5 +3567,4 @@ class HTTP_Response
         }
         return $unpacked;
     }
-} // End class HTTP_Response
-?>
+}
